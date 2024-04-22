8bd359fcc3f25d05aa145d2fe5fc48854d768acf
Andreea Dutia <andreea.balu@gmail.com>
2024-04-22 11:36:52 +0300

    curs15 final

--- src/main/java/curs15/CssSelectorExample.java
+++ src/main/java/curs15/CssSelectorExample.java
@@ -1,5 +1,7 @@
 package curs15;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
@@ -81,8 +83,43 @@ public class CssSelectorExample extends BaseTest {
 		WebElement book4 = driver.findElement(By.cssSelector("a[href$='story']"));
 		System.out.println(book4.getText());
 		
+			}
+	
+	@Test
+	public void cssExample4() throws InterruptedException {
+		JavascriptExecutor jse = (JavascriptExecutor) driver;
+		//driver child in css : >
+		List<WebElement> menuEntries = driver.findElements(By.cssSelector("ul[role='tablist']>li"));
+		jse.executeScript("arguments[0].setAttribute('style', 'border:gpx solid green;')", menuEntries.get(3));
 		
+		WebElement selectedMenuEntry = driver.findElement(By.cssSelector("ul[role='tablist']>li[aria-selected='true']"));
+		jse.executeScript("arguments[0].setAttribute('style', 'border:6px solid blue;')", selectedMenuEntry);
 		
+		List<WebElement> notSelectedmenuEntries = driver.findElements(By.cssSelector("ul[role='tablist']>li:not([aria-selected='true'])"));
+		
+				for (WebElement element : notSelectedmenuEntries) {
+					Thread.sleep(3000);
+					jse.executeScript("arguments[0].setAttribute('style', 'border:6px solid orange;')", element);
+			
 		}
+				
+				/*
+				 * ancestor
+				 * 
+				 * ul[role='tablist']>li[role='tab']> a[href*='#sc_tab']
+				 * ul --> parinte al lui li
+				 * li --> copil al lui ul si parinte al lui a
+				 * a --> copil al lui li si nepot al lui ul
+				 * ca sa cobor de la UL direct la A, adica sa sar peste LI care este la mijloc, nu mai fac referinta cu > (direct child) ci pun doar un spatiu gol
+				 * ul[role='tablist'] a[href*='#sc_tab']
+				 * 
+				 */
+				
+				List<WebElement> menuLinks = driver.findElements(By.cssSelector("ul[role='tablist'] a[href*='#sc_tab']"));
+				for (WebElement element : menuLinks) {
+					Thread.sleep(3000);
+					jse.executeScript("arguments[0].setAttribute('style', 'border:6px solid pink;')", element);
+				}
+	}
 
 }
---
+++ src/main/java/curs15/FindElementsExample.java
@@ -0,0 +1,25 @@
+package curs15;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+import seleniumutils.BaseTest;
+
+public class FindElementsExample extends BaseTest {
+	
+	@Test
+	public void findElementsTest() {
+		
+		JavascriptExecutor jse = (JavascriptExecutor) driver;
+		
+		List<WebElement> bookPicture = driver.findElements(By.cssSelector("figure[class*='sc_image_shape_square']"));
+		
+		jse.executeScript("arguments[0].setAttribute('style', 'border:4px solid red;')", bookPicture.get(1));
+		
+	}
+
+}
