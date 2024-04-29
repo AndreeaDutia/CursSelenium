cbcd2de2391db1de1791b393fcfbb21ca82e4517
Andreea Dutia <andreea.balu@gmail.com>
2024-04-29 10:37:01 +0300

    curs17 intermediar

---
+++ src/main/java/curs17/NavigationExample.java
@@ -0,0 +1,24 @@
+package curs17;
+
+import org.testng.annotations.Test;
+
+import seleniumutils.BaseTest;
+
+public class NavigationExample extends BaseTest {
+	
+	@Test
+	public void navigationTest() throws InterruptedException {
+		
+		Thread.sleep(3000);
+		driver.get("https://emag.ro");
+		Thread.sleep(3000);
+		driver.navigate().to("http://altex.ro");
+		Thread.sleep(3000);
+		driver.navigate().back();
+		Thread.sleep(3000);
+		driver.navigate().forward();
+		Thread.sleep(3000);
+		driver.navigate().refresh();
+	}
+
+}
